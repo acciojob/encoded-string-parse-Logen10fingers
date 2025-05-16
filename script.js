9d4f17232a4ace37722106623f2fdf8c02f10831
Logen11fingers <126648995+Logen10fingers@users.noreply.github.com>
2025-05-16 11:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const parseCode = (str) => {
-  // your code here
+  // Split using one or more zeros as the delimiter
+  const parts = str.split(/0+/);
+  return {
+    firstName: parts[0],
+    lastName: parts[1],
+    id: parts[2]
+  };
 };
 
 // Do not change the code below
